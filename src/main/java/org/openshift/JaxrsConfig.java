a8bd49aeddaedcb4a04427f396e105a059d596c8
Matthew Fischer <matt@edamsolutions.com>
2021-03-03 13:31:58 -0500

    Updated API endpoint items.

---
+++ src/main/java/org/openshift/JaxrsConfig.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.openshift;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Matt
+ *
+ */
+
+@ApplicationPath("/api")
+public class JaxrsConfig extends Application {
+
+}
